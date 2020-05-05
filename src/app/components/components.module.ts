daec5d2086f8e42bd76bfa1e94c6816a3d758d3b
kuron334 <kuron.yama@gmail.com>
2020-05-05 18:38:35 +0900

    動作確認

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ListItemComponent } from './list-item/list-item.component';
 
 import { TaskDetailComponent } from '@/components/task-detail/task-detail.component';
 
@@ -9,11 +10,13 @@ import { TaskDetailComponent } from '@/components/task-detail/task-detail.compon
   declarations: [
     // HogeComponent
     TaskDetailComponent,
+    ListItemComponent
   ],
   imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
   exports: [
     // HogeComponent
     TaskDetailComponent,
+    ListItemComponent
   ],
 })
 export class ComponentsModule {}
