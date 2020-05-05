0f029e4cf5403bea4123f03c4845747c9839ed21
kuron334 <kuron.yama@gmail.com>
2020-05-05 18:38:35 +0900

    動作確認

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,14 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
     // HogeComponent
+    ListItemComponent
   ],
   imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
   exports: [
     // HogeComponent
+    ListItemComponent
   ],
 })
 export class ComponentsModule {}
