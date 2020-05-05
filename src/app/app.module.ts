24669e703f897568cf6f0e3fd9f107d0428bcd3d
john <johnbardera@gmail.com>
2020-05-05 18:01:01 +0900

    fixed mistake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { StoreModule } from '@ngrx/store';
+
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {ComponentsModule} from '@/components/components.module';
 
-import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from '../http-interceptors';
-
 import { ApisModule } from './apis/apis.module';
-import { StoreModule } from '@ngrx/store';
-import { reducers } from './stores';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { ServicesModule } from './services/services.module';
+import { reducers } from './stores';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,6 +29,7 @@ import { ServicesModule } from './services/services.module';
     ApisModule,
     StoreModule.forRoot(reducers),
     ServicesModule,
+    ComponentsModule,
   ],
   providers: [
     StatusBar,
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,42 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-import { DirectivesModule } from '@/directives/directives.module';
-import { PipesModule } from '@/pipes/pipes.module';
-
-import { EmptyStateComponent } from './empty-state/empty-state.component';
-import { MemberItemComponent } from './member-item/member-item.component';
-import { NotificationItemComponent } from './notification-item/notification-item.component';
-import { ProductItemComponent } from './product-item/product-item.component';
-import { ProfileFormModalComponent } from './profile-form-modal/profile-form-modal.component';
-import { ScoreTableComponent } from './score/score-table/score-table.component';
-import { AppButtonComponent } from './utils/app-button/app-button.component';
-import { FabButtonComponent } from './utils/fab-button/fab-button.component';
-import { WysiwygBodyComponent } from './wysiwyg-body/wysiwyg-body.component';
-
 @NgModule({
   declarations: [
-    AppButtonComponent,
-    FabButtonComponent,
-    ScoreTableComponent,
-    ProductItemComponent,
-    ProfileFormModalComponent,
-    MemberItemComponent,
-    NotificationItemComponent,
-    WysiwygBodyComponent,
-    EmptyStateComponent,
+    // HogeComponent
   ],
-  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, DirectivesModule, PipesModule],
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
   exports: [
-    AppButtonComponent,
-    FabButtonComponent,
-    ScoreTableComponent,
-    ProductItemComponent,
-    ProfileFormModalComponent,
-    MemberItemComponent,
-    NotificationItemComponent,
-    WysiwygBodyComponent,
-    EmptyStateComponent,
+    // HogeComponent
   ],
 })
 export class ComponentsModule {}
